c81af341c9852a1e15fd52f05fbdd591f79d343b
temploumbandasagrada-stack <temploumbandasagrada@gmail.com>
2026-08-12 22:32:39 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 //
 // >>> Toda vez que subir arquivo novo, troque o número aqui embaixo <<<
 // ============================================================
-const VERSAO = 'casa-de-estudo-v16';
+const VERSAO = 'casa-de-estudo-v17';
 
 const ESSENCIAIS = [
   './',
